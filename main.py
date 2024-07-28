939dcd89c4657856be87f48b6694e2139c52cb2f
Subarna Bhowmik <subarnabhowmik@Subarnas-MacBook-Air.local>
2024-07-28 22:31:58 +0530

    Updated new changes

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ app.add_middleware(
 app.add_middleware(TrustedHostMiddleware, allowed_hosts=['*'])
 app.add_middleware(HTTPSRedirectMiddleware)
 app.add_middleware(GZipMiddleware, minimum_size=1000)
-app.add_middleware(LoggingMiddleware)
+# app.add_middleware(LoggingMiddleware)
 app.add_middleware(CSRFProtectMiddleware, csrf_secret=os.getenv('CSRF_SECRET_KEY'))
 app.add_middleware(RateLimitMiddleware, max_requests=10, window_seconds=60)
 app.add_middleware(SecurityHeadersMiddleware)
