67c699ba52c4ce5fb275130e8ed9a6cb3016ed67
Subarna Bhowmik <subarnabhowmik@Subarnas-MacBook-Air.local>
2024-07-28 22:54:30 +0530

    Commented conflicting logger

--- middlewares/logging_middleware.py
+++ middlewares/logging_middleware.py
@@ -8,16 +8,16 @@ from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.responses import Response
 from typing import Callable, Awaitable
 
-log_file_path = os.path.join(os.getcwd(),'logs','middleware_logs','logging_middleware.log')
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    handlers=[
-        RotatingFileHandler(log_file_path, maxBytes=10485760, backupCount=3),
-        logging.StreamHandler() 
-    ]
-)
-logger = logging.getLogger(__name__)
+# log_file_path = os.path.join(os.getcwd(),'logs','middleware_logs','logging_middleware.log')
+# logging.basicConfig(
+#     level=logging.INFO,
+#     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+#     handlers=[
+#         RotatingFileHandler(log_file_path, maxBytes=10485760, backupCount=3),
+#         logging.StreamHandler() 
+#     ]
+# )
+# logger = logging.getLogger(__name__)
 
 
 class LoggingMiddleware(BaseHTTPMiddleware):
@@ -27,10 +27,10 @@ class LoggingMiddleware(BaseHTTPMiddleware):
         request_id = str(uuid.uuid1().hex)
         request.state.request_id = request_id
 
-        logger.info(f'Request ID: {request_id} - {request.method} {request.url}')
+        # logger.info(f'Request ID: {request_id} - {request.method} {request.url}')
         start_time = time.time()
         response = await call_next(request)
         process_time = time.time() - start_time
-        logger.info(f'Request ID: {request_id} - Response: {response.status_code} (processed in {process_time:.4f} seconds)')
+        # logger.info(f'Request ID: {request_id} - Response: {response.status_code} (processed in {process_time:.4f} seconds)')
 
         return response
